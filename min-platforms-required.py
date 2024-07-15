9cc34214fca846ec1d0c29e0d4cef8ff8b5219a5
Anna Ann Mathew <112563080+AnnaTheSloth284@users.noreply.github.com>
2024-07-15 10:02:44 +0530

    Create min-platforms-required.py

---
+++ min-platforms-required.py
@@ -0,0 +1,12 @@
+# Given the arrival and departure times of all trains that reach a railway station, the task is to find the minimum number of platforms required for the railway 
+# station so that no train waits.We are given 2 arrays that represent the arrival and departure times of trains that stop.
+
+# Input:
+
+# 900 940 950 1100 1500 1800
+
+# 910 1200 1120 1130 1900 2000
+
+# Output:
+
+# Minimum no of platforms needed: 3
